6f3d3677f13655f9d22ea06d602b6044d2948e61
lredwine <lar348@msstate.edu>
2023-07-31 17:52:53 -0400

    Temp Commit for Plants:

--- PlantHospitalMain.py
+++ PlantHospitalMain.py
@@ -3,20 +3,46 @@ import json
 import threading
 import time
 import requests
+import http.client
+"""
+# system library for getting the command line argument
+import sys
+# web library
+import http.client
+def send( message ):
+    # your webhook URL
+    webhookurl = "https://discordapp.com/api/webhooks/YOURWEBHOOK"
+    # compile the form data (BOUNDARY can be anything)
+    formdata = "------:::BOUNDARY:::\r\nContent-Disposition: form-data; name=\"content\"\r\n\r\n" + message + "\r\n------:::BOUNDARY:::--"
+    # get the connection and make the request
+    connection = http.client.HTTPSConnection("discordapp.com")
+    connection.request("POST", webhookurl, formdata, {
+        'content-type': "multipart/form-data; boundary=----:::BOUNDARY:::",
+        'cache-control': "no-cache",
+        })
+    # get the response
+    response = connection.getresponse()
+    result = response.read()
+    # return back to the calling function with the result
+    return result.decode("utf-8")
+# send the messsage and print the response
+print( send( sys.argv[1] ) )
+"""
 
 def send_json_request(ws, request):
     ws.send(json.dumps(request))
 
-def send_json_message(message, token, channelid):
-    payload = {
-        "content": message
-    }
+def send_json_message(message):
 
-    headers = {
-        "authorization": token
-    }
-
-    r = requests.post(f"https://discord.com/api/v8/channels/{channelid}/messages", headers=headers, data=payload)
+    webhookurl = "https://discord.com/api/webhooks/1135662267338326017/NuHNa6839rqJHt3esRFxmG5APow7z3aMluMxOmEVjyDyakrAUG_GmfxQrgEb2z5ne0V-"
+    # compile the form data (BOUNDARY can be anything)
+    formdata = "------:::BOUNDARY:::\r\nContent-Disposition: form-data; name=\"content\"\r\n\r\n" + message + "\r\n------:::BOUNDARY:::--"
+    # get the connection and make the request
+    connection = http.client.HTTPSConnection("discord.com")
+    connection.request("POST", webhookurl, formdata, {
+        'content-type': "multipart/form-data; boundary=----:::BOUNDARY:::",
+        'cache-control': "no-cache",
+    })
 
 def receive_json_response(ws):
     response = ws.recv()
@@ -39,11 +65,11 @@ ws = websocket.WebSocket()
 ws.connect("wss://gateway.discord.gg/?v=6&encording=json")
 event = receive_json_response(ws)
 
-heartbeat_interval = event["d"]["heartbeat_interval"] / 5000
+heartbeat_interval = event["d"]["heartbeat_interval"] / 10000
 threading._start_new_thread(heartbeat, (heartbeat_interval, ws))
 #threading._start_new_thread(test_send_json_request, ())
 
-token = "MzMzMzA4NDYzOTI3MTMyMTcx.X-wv-w.jc9TJJv01ubtdXdn-rzauAkgMVA"
+token = "MzMzMzA4NDYzOTI3MTMyMTcx.G_byfH.ShH73j3rN0-ahjyyu7nH6Avhim5yQjoJ3uN794"
 payload = {
     "op": 2,
     "d": {
@@ -66,7 +92,8 @@ while True:
         print(response)
 
         if response.startswith("ICE"):
-            send_json_message("CREAM", token, "1135657445532909621")
+            print("should")
+            send_json_message("CREAM")
 
         op_code = event("op")
         if op_code == 11:
