ed8b6f5989dff587db6db2e891ee3fcb1474bfc4
lredwine <lar348@msstate.edu>
2023-08-06 13:32:46 -0400

    Created PlantHospital Class

--- PlantHospitalMain.py
+++ PlantHospitalMain.py
@@ -1,52 +1,85 @@
 import websocket
 import json
-import threading
+from threading import Thread
 import time
 import requests
 import http.client
 
-def send_json_request(ws, request):
-    ws.send(json.dumps(request))
 
-def send_json_message(message):
+class PlantHospital():
 
-    webhookurl = "https://discord.com/api/webhooks/1135662267338326017/NuHNa6839rqJHt3esRFxmG5APow7z3aMluMxOmEVjyDyakrAUG_GmfxQrgEb2z5ne0V-"
+    def __init__(self, ws):
+        self.delay = 1
+        self.name = "Plant"
+        self.thirst_bound = 1
+        self.ws = ws
 
-    data = {"content": message}
-    response = requests.post(webhookurl, json=data)
+        self.cycle = True
 
-    print(response.status_code)
+        self.thread = Thread(target=self.thirst_wait)
+        self.thread.start()
 
-    print(response.content)
+    def get_delay(self):
+        return self.delay
 
+    def set_delay(self, delay):
+        self.delay = float(delay)
+        self.cycle = False
+        self.thread.join()
+        self.cycle = True
+        self.thread = Thread(target=self.thirst_wait)
+        self.thread.start()
 
+    def get_name(self):
+        return self.name
 
-def receive_json_response(ws):
-    response = ws.recv()
-    if response:
-        return json.loads(response)
+    def set_name(self, name):
+        self.name = name
 
-def heartbeat(interval, ws):
-    print("Heartbeat begin")
-    while True:
-        time.sleep(interval)
-        heartbeatJSON = {
-            "op": 1,
-            "d": "null",
-        }
+    def get_thirst(self):
+        return self.thirst_bound
 
-        send_json_request(ws, heartbeatJSON)
-        print("Heartbeat Sent")
+    def set_thirst(self, thirst):
+        self.thirst_bound = float(thirst)
+
+    def thirst_reporting(self):
+        return 2
+
+    def thirst_wait(self):
+        start_time = time.time()
+        while self.cycle:
+            current_time = time.time()
+            if (current_time - start_time) > (self.get_delay() * 3600):
+                start_time = time.time()
+                self.send_json_message("My current thirst level is " + str(self.thirst_reporting()))
+
+    def send_json_request(self, request):
+        self.ws.send(json.dumps(request))
+
+    def send_json_message(self, message):
+
+        webhookurl = "https://discord.com/api/webhooks/1135662267338326017/NuHNa6839rqJHt3esRFxmG5APow7z3aMluMxOmEVjyDyakrAUG_GmfxQrgEb2z5ne0V-"
+
+        data = {"content": message}
+        response = requests.post(webhookurl, json=data)
+
+    def receive_json_response(self):
+        response = self.ws.recv()
+        if response:
+            return json.loads(response)
 
 ws = websocket.WebSocket()
 ws.connect("wss://gateway.discord.gg/?v=6&encording=json")
-event = receive_json_response(ws)
+
+planthospital = PlantHospital(ws)
+
+event = planthospital.receive_json_response()
 
 #heartbeat_interval = event["d"]["heartbeat_interval"] / 20000
 #threading._start_new_thread(heartbeat, (heartbeat_interval, ws))
 #threading._start_new_thread(test_send_json_request, ())
 
-token = "MTEzNTc3MDAyMTU0NTY2MDUwNw.Gl5Wza.HdfeodN8Aa4MnhMVNnpVayOorMxibdU23eseyo"
+token = "MTEzNTc3MDAyMTU0NTY2MDUwNw.GG8OjP.ujOCpVaJjrj3X5UYBZScmopKdGsJBnVKv6sFgs"
 payload = {
     "op": 2,
     "d": {
@@ -59,20 +92,43 @@ payload = {
     }
 }
 
-send_json_request(ws, payload)
+planthospital.send_json_request(payload)
+
+#threading._start_new_thread(thirst_wait, (planthospital))
+
 
 while True:
 
 
     try:
 
-        event = receive_json_response(ws)
+        event = planthospital.receive_json_response()
 
         response = f"{event['d']['content']}"
         print(response)
 
-        if response.startswith("ICE"):
-            send_json_message("CREAM")
+        if response.startswith("NAME"):
+            planthospital.set_name(response.split(": ")[1])
+            planthospital.send_json_message("My name is now " + planthospital.get_name())
+
+        if response.startswith("DELAY"):
+            planthospital.set_delay(response.split(": ")[1])
+            planthospital.send_json_message("My reporting delay is now " + str(planthospital.get_delay()) + " hours")
+
+        if response.startswith("THIRST"):
+            planthospital.set_thirst(response.split(": ")[1])
+            planthospital.send_json_message("My reporting thirst level is now " + str(planthospital.get_thirst()))
+
+        if response.startswith("GET"):
+            if response.split(": ")[1] == "NAME":
+                planthospital.send_json_message("My name is " + planthospital.get_name())
+            if response.split(": ")[1] == "DELAY":
+                planthospital.send_json_message("My reporting delay is " + str(planthospital.get_delay()) + " hours")
+            if response.split(": ")[1] == "THIRST":
+                planthospital.send_json_message("My reporting thirst level is " + str(planthospital.get_thirst()))
+
+        if response.startswith("STATUS"):
+            planthospital.send_json_message("My current thirst reading is " + str(planthospital.thirst_reporting()))
 
         op_code = event("op")
         if op_code == 11:
@@ -83,12 +139,16 @@ while True:
         ws = websocket.WebSocket()
         ws.connect("wss://gateway.discord.gg/?v=6&encording=json")
 
-        event = receive_json_response(ws)
+        planthospital.ws = ws
 
-        send_json_request(ws, payload)
+        event = planthospital.receive_json_response()
+
+        planthospital.send_json_request(payload)
 
         pass
 
-    except:
+    except Exception as e:
+
+        print(e)
 
         pass
